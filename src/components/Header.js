dcdde5908b635199bfd9072aed10e2c3fc2153bb
THRY <thierry.marc.seiler@gmail.com>
2018-11-16 20:53:35 +0100

    huhu

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <p>
+          <span>Home</span>
+          <span>Login</span>
+        </p>
+      </header>
+    )
+  }
+}
+
+export default Header;
